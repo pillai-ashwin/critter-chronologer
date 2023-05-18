1005405caadbc99c1517b8a6ab2ab34a3d151bb5
Ashwin Pillai <13049220+pillai-ashwin@users.noreply.github.com>
2023-05-17 21:53:24 -0400

    design customer and pet entity

---
+++ src/main/java/com/udacity/jdnd/course3/critter/pet/Pet.java
@@ -0,0 +1,26 @@
+package com.udacity.jdnd.course3.critter.pet;
+
+import com.udacity.jdnd.course3.critter.user.Customer;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.time.LocalDate;
+
+@Data
+@Entity
+@NoArgsConstructor
+public class Pet implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private PetType type;
+    private String name;
+
+    @ManyToOne(targetEntity = Customer.class, optional = false)
+    private Customer customer;
+    private LocalDate birthDate;
+    private String notes;
+}
---
+++ src/main/java/com/udacity/jdnd/course3/critter/user/Customer.java
@@ -0,0 +1,30 @@
+package com.udacity.jdnd.course3.critter.user;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+import com.udacity.jdnd.course3.critter.pet.Pet;
+
+import java.io.Serializable;
+import java.util.List;
+
+@Data
+@Entity
+@NoArgsConstructor
+public class Customer implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String phoneNumber;
+    private String notes;
+    private String name;
+
+    @OneToMany(targetEntity = Pet.class)
+    private List<Pet> pets;
+
+    public void insertPet(Pet pet) {
+        pets.add(pet);
+    }
+}
