da6ac765631aabcf33bf67067b2eef6ed85a6ab9
pillai-ashwin <pillaiashwin96@gmail.com>
2023-05-31 13:10:42 -0400

    create pet controller and service

--- src/main/java/com/udacity/jdnd/course3/critter/pet/PetController.java
+++ src/main/java/com/udacity/jdnd/course3/critter/pet/PetController.java
@@ -1,8 +1,10 @@
 package com.udacity.jdnd.course3.critter.pet;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Handles web requests related to Pets.
@@ -11,14 +13,34 @@ import java.util.List;
 @RequestMapping("/pet")
 public class PetController {
 
+    @Autowired
+    private PetService petService;
+
+    private PetDTO getPetsDTO(Pet pet) {
+        PetDTO petDTO = new PetDTO();
+        petDTO.setId(pet.getId());
+        petDTO.setBirthDate(pet.getBirthDate());
+        petDTO.setName(pet.getName());
+        petDTO.setNotes(pet.getNotes());
+        petDTO.setOwnerId(pet.getCustomer().getId());
+        petDTO.setType(pet.getType());
+        return petDTO;
+    }
+
     @PostMapping
     public PetDTO savePet(@RequestBody PetDTO petDTO) {
-        throw new UnsupportedOperationException();
+        Pet pet = new Pet();
+        pet.setType(petDTO.getType());
+        pet.setBirthDate(petDTO.getBirthDate());
+        pet.setName(petDTO.getName());
+        pet.setNotes(petDTO.getNotes());
+        return getPetsDTO(petService.savePet(pet, petDTO.getOwnerId()));
+
     }
 
     @GetMapping("/{petId}")
     public PetDTO getPet(@PathVariable long petId) {
-        throw new UnsupportedOperationException();
+        return getPetsDTO(petService.getPetById(petId));
     }
 
     @GetMapping
@@ -28,6 +50,7 @@ public class PetController {
 
     @GetMapping("/owner/{ownerId}")
     public List<PetDTO> getPetsByOwner(@PathVariable long ownerId) {
-        throw new UnsupportedOperationException();
+        List<Pet> pets = petService.getPetsByCustomerId(ownerId);
+        return pets.stream().map(this::getPetsDTO).collect(Collectors.toList());
     }
 }
---
+++ src/main/java/com/udacity/jdnd/course3/critter/pet/PetService.java
@@ -0,0 +1,36 @@
+package com.udacity.jdnd.course3.critter.pet;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.udacity.jdnd.course3.critter.user.Customer;
+import com.udacity.jdnd.course3.critter.user.CustomerRepository;
+
+@Service
+public class PetService {
+    
+    @Autowired
+    private PetRepository petRepository;
+
+    @Autowired
+    private CustomerRepository customerRepository;
+
+    public Pet savePet(Pet pet, Long ownerId) {
+        Customer customer = customerRepository.getOne(ownerId);
+        pet.setCustomer(customer);
+        pet = petRepository.save(pet);
+        customer.insertPet(pet);
+        customerRepository.save(customer);
+        return pet;
+    }
+
+    public Pet getPetById(Long petId){
+        return petRepository.getOne(petId);
+    }
+
+    public List<Pet> getPetsByCustomerId(Long customerId){
+        return petRepository.getAllByCustomerId(customerId);
+    }
+}
